89cf94a2b5df4bcbe9ba7ae47bd434a8b616ac48
bombehub <liliang@stumail.neu.edu.cn>
2019-03-12 14:44:02 +0800

    fix

--- build.sh
+++ build.sh
@@ -1,39 +1,31 @@
 cd cou-redis-3.0/deps/
 rm -rf jemalloc
-git clone https://github.com/jemalloc/jemalloc.git
-cd jemalloc
-./autogen.sh
-cd ..
+tar xjvf ../../jemalloc-5.1.0.tar.bz2
+mv jemalloc-5.1.0/ jemalloc
 make jemalloc
 cd ..
 make
 cd ..
 cd ll-redis-3.0/deps/
 rm -rf jemalloc
-git clone https://github.com/jemalloc/jemalloc.git
-cd jemalloc
-./autogen.sh
-cd ..
+tar xjvf ../../jemalloc-5.1.0.tar.bz2
+mv jemalloc-5.1.0/ jemalloc
 make jemalloc
 cd ..
 make
 cd ..
 cd mk-redis-3.0/deps/
 rm -rf jemalloc
-git clone https://github.com/jemalloc/jemalloc.git
-cd jemalloc
-./autogen.sh
-cd ..
+tar xjvf ../../jemalloc-5.1.0.tar.bz2
+mv jemalloc-5.1.0/ jemalloc
 make jemalloc
 cd ..
 make
 cd ..
 cd zz-redis-3.0/deps/
 rm -rf jemalloc
-git clone https://github.com/jemalloc/jemalloc.git
-cd jemalloc
-./autogen.sh
-cd ..
+tar xjvf ../../jemalloc-5.1.0.tar.bz2
+mv jemalloc-5.1.0/ jemalloc
 make jemalloc
 cd ..
 make
